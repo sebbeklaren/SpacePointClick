2e72a85a5276539171cf5d0c81aee866f440320f
sebbeklaren <sebbeklaren@gmail.com>
2017-07-01 19:11:38 +0200

    Nytt

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Vector2 cameraTarget;
+    public Transform target;
+
+    void Start()
+    {
+        //target = GameObject.FindGameObjectWithTag("Boss").transform;
+
+    }
+
+
+    void Update()
+    {
+        cameraTarget = new Vector3(target.position.x + 6, transform.position.y, -15);
+        //transform.position = Vector3.Lerp(transform.position, cameraTarget, Time.deltaTime * 8);
+        transform.position = new Vector3(cameraTarget.x, cameraTarget.y, -15);
+    }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+    private float walkSpeed = 2f;
+    public float runSpeed = 4;
+    private CharacterController controller;
+    private Camera camera;
+    private Vector2 mousClickPos;
+    private float speed = 2;
+    private float distanceToClick;
+    void Start ()
+    {
+        controller = GetComponent<CharacterController>();
+        mousClickPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ControllWASD();
+        distanceToClick = 0;
+    }
+
+    void ControllWASD()
+    {
+        camera = Camera.main;
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if(Input.GetKeyDown("w"))
+        {
+            input.y += 8;
+        }
+        else
+        {
+            input.y = 0;
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+          {
+             mousClickPos = camera.ScreenToWorldPoint(Input.mousePosition);
+            //distanceToClick = Input.mousePosition.x;
+            Debug.Log("this.x: " + transform.position.x + "mousepos: " + mousClickPos);
+        }
+        if (mousClickPos.x != transform.position.x)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mousClickPos, speed * Time.deltaTime);
+        }
+        else
+        {
+            
+        }
+
+       
+
+        //transform.position = transform.position + input;
+        //currentVolicityMod = Vector2.MoveTowards(currentVolicityMod, input, acceleration * Time.deltaTime);
+        Vector2 motion = input;
+       // motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.y) == 1) ? 0.7f : 1;
+        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+
+        motion += Vector2.up * -1;
+        controller.Move(motion * Time.deltaTime);
+    }
+}
