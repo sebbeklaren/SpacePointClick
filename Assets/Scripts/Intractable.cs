b5072a34e215d176593229ba94eff8e363160aaa
sebbeklaren <sebbeklaren@gmail.com>
2017-07-31 10:14:27 +0200

    Nya sprites

---
+++ Assets/Scripts/Intractable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    bool interactable;
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
